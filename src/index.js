7e4da899c63f511be5d87057d062e91711cf92e4
nazisang@gmail.com <bill.xia@team.telstra.com>
2020-11-20 14:57:19 +1100

    update competition methods

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 class Square extends React.Component {
   constructor(props) {
     super(props)
+    /*Build in variables: state, props*/
     this.state = {
       value:null
     }
@@ -13,22 +14,48 @@ class Square extends React.Component {
     return (
       <button 
       className="square"
-      onClick={() => this.setState({value: "x"})}
+      onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    /*slice() 方法返回一个新的数组对象，这一对象是一个由 begin 和 end 决定的原数组的浅拷贝*/
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares:squares,
+      xIsNext: !this.state.xIsNext,
+    });
+
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+     value={this.state.squares[i]}
+     onClick = {()=> this.handleClick(i)}
+     />;
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares)
+    let status;
+    if(winner){
+      status = 'winner: ' + winner
+    } else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -69,6 +96,27 @@ class Game extends React.Component {
 }
 
 // ========================================
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(a,b,c);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null
+}
 
 ReactDOM.render(
   <Game />,
